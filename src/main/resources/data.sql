2fa21a26f5fe2bdab6289eece6d7ce57c9137fb9
Nathalia Ramos <nathii.siilva@gmail.com>
2023-07-03 05:18:42 -0300

    Alterando o arquivo de alimentação do banco de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
-INSERT INTO customer (id, name, email) VALUES
-(1, 'Mariazinha', 'mariazinha@email.com'),
-(2, 'Joãozinho', 'joaozinho@email.com');
+INSERT INTO customer (id, name) VALUES
+(1, 'Maria'),
+(2, 'João'),
+(3, 'Lucas'),
+(4, 'Fernanda');
+
+INSERT INTO email (id, customer_id, email) VALUES
+(1, 1, 'mariazinha@email.com'),
+(2, 2, 'joaozinho@email.com'),
+(3, 3, 'lu@email.com'),
+(4, 4, 'fernanda@email.com'),
+(5, 4, 'fefe@email.com');
